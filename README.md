2c5a2b99b2275dab0aa32293bc1902fa46459b8f
NaliniSekar <NaliniSekar@users.noreply.github.com>
2016-07-18 00:36:17 -0700

    Update and rename README.md to largeof3.java

--- README.md
+++
@@ -1,1 +0,0 @@
-# largeof3
---
+++ largeof3.java
@@ -0,0 +1,26 @@
+# largeof3
+Public static void main(String args[])
+{
+System.out.println("enter num");
+Scanner in = new Scanner();
+int a=in.nextInt();
+System.out.println("enter num");
+Scanner in = new Scanner();
+int b=in.nextInt();
+System.out.println("enter num");
+Scanner in = new Scanner();
+int c=in.nextInt();
+if(a>b&&a>c)
+{
+System.out.println("a is greater");
+}
+elseif(b>a&&b>c)
+{
+System.out.println("b is greater");
+}
+elseif(c>a&&c>b)
+{
+System.out.println("c is greater");
+}
+
+}
